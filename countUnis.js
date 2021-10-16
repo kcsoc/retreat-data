4f3f999d45786adfcd6630ae864623dab2608bc9
Dhaval Soneji <dhaval@soneji.xyz>
2021-10-16 13:34:37 +0100

    Update

--- countUnis.js
+++ countUnis.js
@@ -1,21 +1,29 @@
-const unis = require("./unis.json");
+// const unis = require("./unis.json");
+const csvFilePath = "./orders.csv";
+const { csv2json } = require("./csv2json");
 const key = "Product Form: University";
 
-const result = [];
+(async () => {
+  const unis = await csv2json(csvFilePath);
+  // console.log(unis);
 
-unis.forEach((student) => {
-  const uni = student[key];
-  const obj = result.find((obj) => obj.uni === uni);
-  if (!obj) {
-    result.push({
-      uni,
-      value: 1,
-    });
-  } else {
-    obj.value++;
-  }
-});
+  
+  const result = [];
 
-result.sort((a, b) => (a.value < b.value ? 1 : -1));
+  unis.forEach((student) => {
+    const uni = student[key];
+    const obj = result.find((obj) => obj.uni === uni);
+    if (!obj) {
+      result.push({
+        uni,
+        value: 1,
+      });
+    } else {
+      obj.value++;
+    }
+  });
 
-console.log(result);
+  result.sort((a, b) => (a.value < b.value ? 1 : -1));
+
+  console.log(result);
+})();
